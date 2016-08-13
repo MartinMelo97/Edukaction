84c2cb405fcdf885c8245344a0ebd7c838a83427
ozobear <fuckbear@MacBook-Pro-de-Oscar.local>
2016-08-13 12:08:18 -0500

    Se completa main y se agrega auth con Facebook(aún no hay app de fb)

--- Edukaction/settings.py
+++ Edukaction/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'social.backends.facebook.Facebook2OAuth2',
+    )
