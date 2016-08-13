5e900c1cf164096d21f8d0853e6590f7312bf90a
ozobear <fuckbear@MacBook-Pro-de-Oscar.local>
2016-08-13 12:06:19 -0500

    Se completa main y se agrega auth con Facebook(aún no hay app de fb)

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import View
 
-# Create your views here.
+class Home(View):
+	def get(self,request):
+		template_name = "main/home.html"
+		context = {}
+		return render(request,template_name,context)
